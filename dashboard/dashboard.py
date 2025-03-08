fc081c8ffe7339e1d3a1a28995d658bf584e8736
Novil-M <112967305+Nvl123@users.noreply.github.com>
2025-03-09 05:12:25 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -276,7 +276,7 @@ def show_profile():
     # Menambahkan deskripsi singkat tentang diri Anda
     st.sidebar.subheader("Nama: Moh. Novil Ma'arij")
     st.sidebar.write("Saya adalah seorang pengembang dashboard dan peneliti data. "
-                     "Saya memiliki latar belakang di bidang [sebutkan bidang studi atau profesi Anda]. "
+                     "Saya memiliki latar belakang di bidang IT. "
                      "Saya membuat dashboard ini untuk memvisualisasikan kualitas udara dan memberikan insight "
                      "terkait polusi udara di berbagai lokasi.")
     
